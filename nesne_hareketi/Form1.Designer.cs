5e39b8d4b3503614c17d19a22400813dc4b8f766
Samet Poyraz <poyraz50_38@hotmail.com>
2020-04-27 14:51:41 +0300

    nesne hareketi programı yüklenmesi

---
+++ nesne_hareketi/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace nesne_hareketi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.panel11 = new System.Windows.Forms.Panel();
+            this.panel12 = new System.Windows.Forms.Panel();
+            this.panel13 = new System.Windows.Forms.Panel();
+            this.panel14 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.timer6 = new System.Windows.Forms.Timer(this.components);
+            this.timer7 = new System.Windows.Forms.Timer(this.components);
+            this.timer8 = new System.Windows.Forms.Timer(this.components);
+            this.timer9 = new System.Windows.Forms.Timer(this.components);
+            this.timer10 = new System.Windows.Forms.Timer(this.components);
+            this.timer11 = new System.Windows.Forms.Timer(this.components);
+            this.timer12 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel1.Location = new System.Drawing.Point(3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(22, 481);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel2.Location = new System.Drawing.Point(793, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(22, 481);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel3.Location = new System.Drawing.Point(24, 2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(771, 22);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel4.Location = new System.Drawing.Point(24, 461);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(771, 22);
+            this.panel4.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel5.Location = new System.Drawing.Point(80, 75);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(22, 386);
+            this.panel5.TabIndex = 4;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel6.Location = new System.Drawing.Point(719, 75);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(22, 339);
+            this.panel6.TabIndex = 5;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel7.Location = new System.Drawing.Point(100, 75);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(620, 22);
+            this.panel7.TabIndex = 6;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel8.Location = new System.Drawing.Point(191, 392);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(528, 22);
+            this.panel8.TabIndex = 7;
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel9.Location = new System.Drawing.Point(169, 149);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(22, 265);
+            this.panel9.TabIndex = 8;
+            // 
+            // panel10
+            // 
+            this.panel10.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel10.Location = new System.Drawing.Point(189, 149);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(453, 22);
+            this.panel10.TabIndex = 9;
+            // 
+            // panel11
+            // 
+            this.panel11.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel11.Location = new System.Drawing.Point(620, 171);
+            this.panel11.Name = "panel11";
+            this.panel11.Size = new System.Drawing.Size(22, 174);
+            this.panel11.TabIndex = 10;
+            // 
+            // panel12
+            // 
+            this.panel12.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel12.Location = new System.Drawing.Point(248, 323);
+            this.panel12.Name = "panel12";
+            this.panel12.Size = new System.Drawing.Size(372, 22);
+            this.panel12.TabIndex = 11;
+            // 
+            // panel13
+            // 
+            this.panel13.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel13.Location = new System.Drawing.Point(248, 219);
+            this.panel13.Name = "panel13";
+            this.panel13.Size = new System.Drawing.Size(22, 104);
+            this.panel13.TabIndex = 12;
+            // 
+            // panel14
+            // 
+            this.panel14.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel14.Location = new System.Drawing.Point(270, 219);
+            this.panel14.Name = "panel14";
+            this.panel14.Size = new System.Drawing.Size(282, 22);
+            this.panel14.TabIndex = 13;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(36, 417);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(38, 38);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // timer5
+            // 
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // timer6
+            // 
+            this.timer6.Tick += new System.EventHandler(this.timer6_Tick);
+            // 
+            // timer7
+            // 
+            this.timer7.Tick += new System.EventHandler(this.timer7_Tick);
+            // 
+            // timer8
+            // 
+            this.timer8.Tick += new System.EventHandler(this.timer8_Tick);
+            // 
+            // timer9
+            // 
+            this.timer9.Tick += new System.EventHandler(this.timer9_Tick);
+            // 
+            // timer10
+            // 
+            this.timer10.Tick += new System.EventHandler(this.timer10_Tick);
+            // 
+            // timer11
+            // 
+            this.timer11.Tick += new System.EventHandler(this.timer11_Tick);
+            // 
+            // timer12
+            // 
+            this.timer12.Tick += new System.EventHandler(this.timer12_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(818, 484);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel14);
+            this.Controls.Add(this.panel13);
+            this.Controls.Add(this.panel12);
+            this.Controls.Add(this.panel11);
+            this.Controls.Add(this.panel10);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Panel panel11;
+        private System.Windows.Forms.Panel panel12;
+        private System.Windows.Forms.Panel panel13;
+        private System.Windows.Forms.Panel panel14;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.Timer timer6;
+        private System.Windows.Forms.Timer timer7;
+        private System.Windows.Forms.Timer timer8;
+        private System.Windows.Forms.Timer timer9;
+        private System.Windows.Forms.Timer timer10;
+        private System.Windows.Forms.Timer timer11;
+        private System.Windows.Forms.Timer timer12;
+    }
+}
+
---
+++ nesne_hareketi/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nesne_hareketi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Top -= 5;
+            if (pictureBox1.Top <= 30)
+            {
+                timer1.Stop();
+                timer2.Start();
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left += 5;
+            if (pictureBox1.Left >= 746)
+            {
+                timer2.Stop();
+                timer3.Start();
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Top +=5;
+            if (pictureBox1.Top >= 417)
+            {
+                timer3.Stop();
+                timer4.Start();
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left -= 5;
+            if (pictureBox1.Left <= 110)
+            {
+                timer4.Stop();
+                timer5.Start();
+            }
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Top -= 5;
+            if (pictureBox1.Top <= 105)
+            {
+                timer5.Stop();
+                timer6.Start();
+            }
+        }
+
+        private void timer6_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left += 5;
+            if (pictureBox1.Left >= 660)
+            {
+                timer6.Stop();
+                timer7.Start();
+            }
+        }
+
+        private void timer7_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Top += 5;
+            if (pictureBox1.Top >= 350)
+            {
+                timer7.Stop();
+                timer8.Start();
+            }
+        }
+
+        private void timer8_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left -= 5;
+            if (pictureBox1.Left <= 200)
+            {
+                timer8.Stop();
+                timer9.Start();
+            }
+        }
+
+        private void timer9_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Top -= 5;
+            if (pictureBox1.Top <= 175)
+            {
+                timer9.Stop();
+                timer10.Start();
+            }
+        }
+
+        private void timer10_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left += 5;
+            if (pictureBox1.Left >= 565)
+            {
+                timer10.Stop();
+                timer11.Start();
+            }
+        }
+
+        private void timer11_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Top += 5;
+            if (pictureBox1.Top >= 267)
+            {
+                timer11.Stop();
+                timer12.Start();
+            }
+        }
+
+        private void timer12_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left -= 5;
+            if (pictureBox1.Left <= 276)
+            {
+                timer12.Stop();
+            }
+        }
+    }
+}
